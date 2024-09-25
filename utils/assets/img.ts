5241aa73b1f7996ffe26965eac7dde39ab9efabc
Aleksey <alexsawenkov@gmail.com>
2024-09-25 08:36:37 +0300

    update bs.api url .front

--- utils/assets/img.ts
+++ utils/assets/img.ts
@@ -1,16 +1,19 @@
 export function getImageUrl(name: any) {
+
+    const baseUrl = 'http://185.23.238.13/'
+
     // @ts-ignore
     try {
 
         if (name.includes('http')) {
             return name
         } else {
-            return 'http://localhost:8000/' + name
+            return baseUrl + name
         }
 
         // return new URL(`http://localhost:8080/${name}`, import.meta.url).href
     } catch (e) {
-        return 'http://localhost:8000/static/media/test.png'
+        return baseUrl+'/static/media/test.png'
     }
 
     // return new `http://localhost:8080/static/${import.meta.url}`
