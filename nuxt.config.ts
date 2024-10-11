87bacb488bb8a62320135793361f7041da739068
Aleksey <alexsawenkov@gmail.com>
2024-10-11 12:32:53 +0300

    fix zoom - viewport

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,7 +17,9 @@ export default defineNuxtConfig({
                 lang: 'en'
             },
             meta: [
-                {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+
+                // {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+                {name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'},
                 {property: 'og:site_name', content: 'Sneaker Shop'},
                 // {property: 'og:image', content: ''},
                 // {property: 'og:url', content: ''},
